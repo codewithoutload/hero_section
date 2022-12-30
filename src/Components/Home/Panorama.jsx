f7fe61d4785c31df0608fde4e5fd1dbd45cf1c34
Geetmanik <geetmanik2@gmail.com>
2022-12-30 19:15:28 +0530

    paorama

--- src/Components/Home/Panorama.jsx
+++ src/Components/Home/Panorama.jsx
@@ -3,28 +3,34 @@ import * as PANOLENS from "panolens";
 import image from "./images/image.jpg"
 
 
+
+let panoContainer = document.querySelector(".panolens-container");
+
 const panoramaImage = new PANOLENS.ImagePanorama(image);
-const imageContainer = document.querySelector(".main-container");
+const imageContainer = document.querySelector(".image-container");
+    const viewer = new PANOLENS.Viewer({
+      container: imageContainer,
+      controlBar: false,
+    });
+    viewer.OrbitControls.reverseDragging= true;
+    viewer.OrbitControls.noZoom = true; 
+    viewer.add(panoramaImage);
 
-  
-const viewer = new PANOLENS.Viewer({
-  container: imageContainer,
-  controlBar: false,
-});
 
-viewer.add(panoramaImage);
+
  
-viewer.OrbitControls.reverseDragging= true;
-viewer.OrbitControls.noZoom = true; 
+
   const Panorama = () =>{
+
   return (
     <>
-      <div className="description">Alcheringa is the annual cultural festival of the Indian Institute of Technology, Guwahati. A splendid idea realised by a group of students in 1996 at IITG marked the dawn of one of Asia’s most exhilarating and significant cultural college fests as we know it today. </div>
+     
         <div className="main-container">
-       
-        
-      
-
+   
+        <div className="description">Alcheringa is the annual cultural festival of the Indian Institute of Technology, Guwahati. A splendid idea realised by a group of students in 1996 at IITG marked the dawn of one of Asia’s most exhilarating and significant cultural college fests as we know it today. </div>
+     <div className="image-container">
+         {}
+     </div>
      
         </div>
     </>
