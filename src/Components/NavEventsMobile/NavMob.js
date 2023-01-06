b229585e53640153b1730c1e3a9e949b174db6b3
mohit <mohitsharma55337@gmail.com>
2023-01-07 00:12:34 +0530

    trial

--- src/Components/NavEventsMobile/NavMob.js
+++ src/Components/NavEventsMobile/NavMob.js
@@ -1,3 +1,13 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import './NavBar.css'
+
+const NavMOb = () => {
+    return ( 
+        <div>
+            
+        </div>
+    );
+}
+ 
+export default NavMOb;
