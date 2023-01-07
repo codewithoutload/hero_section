8003240330e37faecec7f4a3d688870b0352e9d9
digvijaysingh3023 <digvijaysihag123@gmail.com>
2023-01-07 19:26:05 +0530

    strips

---
+++ src/Components/Home/Strips.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Strips.css';
+import star from './images/Mask group.png';
+function Strips() {
+  return (
+    <div className='stripContainer'>
+        <div className='strip_container1'>
+        </div>
+            <div className='strip1'>
+                <div className='football_text1'>140K FOOTBALL</div>
+                <div><img className='star star1' src={star}></img></div>
+                <div className='edition_text'>26 EDITIONS</div>
+                <div><img className='star star2' src={star}></img></div>
+                <div className='football_text'>140K FOOTBALL</div>
+                <div><img className='star star3' src={star}></img></div>
+            </div>
+        <div className='strip_container2'>
+        </div>
+            <div className='strip2'>
+                <div className='football_text1'>500+ COLLEGES</div>
+                <div><img className='star star1' src={star}></img></div>
+                <div className='edition_text'>100+ EVENTS</div>
+                <div><img className='star star2' src={star}></img></div>
+                <div className='football_text'>500+ COLLEGES</div>
+                <div><img className='star star3' src={star}></img></div>
+            </div>
+    </div>
+  )
+}
+
+export default Strips;
